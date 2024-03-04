0a8d3d01e4fcdf9d8b5ff7b2e846382149f3e89b
CampBatt <120993447+CampBatt@users.noreply.github.com>
2024-03-03 22:32:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,325 @@
+var map1 = document.getElementById("floor1");
+var cam1 = document.getElementById('cam1');
+var cam2 = document.getElementById('cam2');
+var cam3 = document.getElementById('cam3');
+var cam4 = document.getElementById('cam4');
+var cam5 = document.getElementById('cam5');
+var cam6 = document.getElementById('cam6');
+var cam7 = document.getElementById('cam7');
+var cam8 = document.getElementById('cam8');
+var cam9 = document.getElementById('cam9');
+var cam10 = document.getElementById('cam10');
+var cam11 = document.getElementById('cam11');
+var cam12 = document.getElementById('cam12');
+var cam13 = document.getElementById('cam13');
+var cam14 = document.getElementById('cam14');
+var cam15 = document.getElementById('cam15');
+var cam16 = document.getElementById('cam16');
+var cam17 = document.getElementById('cam17');
+var cam18 = document.getElementById('cam18');
+var cam19 = document.getElementById('cam19');
+var cam20 = document.getElementById('cam20');
+var cam21 = document.getElementById('cam21');
+var cam22 = document.getElementById('cam22');
+var cam23 = document.getElementById('cam23');
+var cam24 = document.getElementById('cam24');
+var cam25 = document.getElementById('cam25');
+var cam26 = document.getElementById('cam26');
+var cam27 = document.getElementById('cam27');
+var cam28 = document.getElementById('cam28');
+var cam29 = document.getElementById('cam29');
+var cam30 = document.getElementById('cam30');
+var cam31 = document.getElementById('cam31');
+var cam32 = document.getElementById('cam32');
+var cam33 = document.getElementById('cam33');
+var cam34 = document.getElementById('cam34');
+var cam35 = document.getElementById('cam35');
+var cam36 = document.getElementById('cam36');
+var cam37 = document.getElementById('cam37');
+var cam38 = document.getElementById('cam38');
+var cam39 = document.getElementById('cam39');
+var cam40 = document.getElementById('cam40');
+var cam41 = document.getElementById('cam41');
+var cam42 = document.getElementById('cam42');
+var cam43 = document.getElementById('cam43');
+var cam44 = document.getElementById('cam44');
+var cam45 = document.getElementById('cam45');
+var cam46 = document.getElementById('cam46');
+var cam47 = document.getElementById('cam47');
+var cam48 = document.getElementById('cam48');
+var cam49 = document.getElementById('cam49');
+var cam50 = document.getElementById('cam50');
+var cam51 = document.getElementById('cam51');
+var cam52 = document.getElementById('cam52');
+var cam53 = document.getElementById('cam53');
+var cam54 = document.getElementById('cam54');
+var cam55 = document.getElementById('cam55');
+var cam56 = document.getElementById('cam56');
+var cam57 = document.getElementById('cam57');
+var cam58 = document.getElementById('cam58');
+var cam59 = document.getElementById('cam59');
+var cam60 = document.getElementById('cam60');
+var cam61 = document.getElementById('cam61');
+var cam62 = document.getElementById('cam62');
+var cam63 = document.getElementById('cam63');
+map1.addEventListener("click",function(event){
+    console.log(event.layerX,event.layerY)
+})
+
+var map2 = document.getElementById("floor2");
+map2.addEventListener("click",function(event){
+    console.log(event.layerX,event.layerY)
+})
+
+cam1.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.27", '_blank');
+});
+cam2.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.201", '_blank');
+});
+cam3.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.202", '_blank');
+});
+cam4.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.202", '_blank');
+});
+cam5.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.202", '_blank');
+});
+cam6.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.203", '_blank');
+});
+cam7.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.201", '_blank');
+});
+cam8.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.203", '_blank');
+});
+cam9.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.204", '_blank');
+});
+cam10.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.204", '_blank');
+});
+cam11.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.33", '_blank');
+});
+cam12.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.204", '_blank');
+});
+cam13.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.34", '_blank');
+});
+cam14.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.208", '_blank');
+});
+cam15.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.207", '_blank');
+});
+cam16.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.207", '_blank');
+});
+cam17.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.66", '_blank');
+});
+cam18.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.65", '_blank');
+});
+cam19.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.206", '_blank');
+});
+cam20.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.205", '_blank');
+});
+cam21.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.208", '_blank');
+});
+cam22.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.57", '_blank');
+});
+cam23.addEventListener("click",function(){
+    window.open("https://10.2.200.59", '_blank');
+});
+cam24.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.28", '_blank');
+});
+cam25.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.69", '_blank');
+});
+cam26.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.61", '_blank');
+});
+cam27.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.60", '_blank');
+});
+cam28.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.55", '_blank');
+});
+cam29.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.68", '_blank');
+});
+cam30.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.29", '_blank');
+});
+cam31.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.46", '_blank');
+});
+cam32.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.30", '_blank');
+});
+cam33.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.70", '_blank');
+});
+cam34.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.31", '_blank');
+});
+cam35.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.32", '_blank');
+});
+cam36.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.212", '_blank');
+});
+cam37.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.211", '_blank');
+});
+cam38.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.212", '_blank');
+});
+cam39.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.209", '_blank');
+});
+cam40.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.210", '_blank');
+});
+cam41.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.203", '_blank');
+});
+cam42.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.56", '_blank');
+});
+cam43.addEventListener("click",function(){
+    alert('administrator,gd5150');
+    window.open("https://10.2.200.72", '_blank');
+});
+cam44.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.43", '_blank');
+});
+cam45.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.45", '_blank');
+});
+cam46.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.71", '_blank');
+});
+cam47.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.211", '_blank');
+});
+cam48.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.210", '_blank');
+});
+cam49.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.211", '_blank');
+});
+cam50.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.63", '_blank');
+});
+cam51.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.75", '_blank');
+});
+cam52.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.211", '_blank');
+});
+cam53.addEventListener("click",function(){
+    alert('admin,gd5150');
+    window.open("https://10.2.200.212", '_blank');
+});
+cam54.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.37", '_blank');
+});
+cam55.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.36", '_blank');
+});
+cam56.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.210", '_blank');
+});
+cam57.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.42", '_blank');
+});
+cam58.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.39", '_blank');
+});
+cam59.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.35", '_blank');
+});
+cam60.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.32", '_blank');
+});
+cam61.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.40", '_blank');
+});
+cam62.addEventListener("click",function(){
+    alert('admin,GD5150');
+    window.open("https://10.2.200.205", '_blank');
+});
+cam63.addEventListener("click",function(){
+    alert('administrator,GD5150');
+    window.open("https://10.2.200.41", '_blank');
+});
+
